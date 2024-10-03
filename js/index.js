c958fd2a82f6d6e225531a57736c9e8a16ecd295
Nkid10 <afonsonkidisala@hotmail.com>
2024-10-03 17:47:01 +0300

    Final version test article

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function Toggle(){
+    let menu = document.getElementById("show");
+
+    if(menu.style.display === 'block'){
+        menu.style.display === 'none'
+        
+    }else{
+        menu.style.display = 'block';
+
+    }
+}
+
+function hideMenu(){
+    let menu = document.getElementById("show");
+    if(menu.style.display === 'block'){
+         menu.style.display === 'none'
+        
+    }else{
+        menu.style.display = 'block';
+    }
+}
